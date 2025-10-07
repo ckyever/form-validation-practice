19580721c7a6c9e9920c200af60f6ca2dfc59018
ckyever <ckyeverg@gmail.com>
2025-10-07 15:50:01 +1100

    Add validation for email field

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+import { Validator } from "./Validator.js";
+
+const email = document.getElementById("email");
+const emailValidator = new Validator(email);
+
+emailValidator.addValidCheck((value) => {
+  return value.length > 0;
+}, "Please enter an email");
+
+emailValidator.addValidCheck((value) => {
+  const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;
+  return emailRegExp.test(value);
+}, "Invalid email (e.g. example@email.com)");
+
+email.addEventListener("blur", emailValidator.isValid);
