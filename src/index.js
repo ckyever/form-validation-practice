c5e5c8c63aa2fac517390518d5dee05d69e9b146
ckyever <ckyeverg@gmail.com>
2025-10-07 15:53:44 +1100

    Add country field validation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { Validator } from "./Validator.js";
 
+// Email validation
 const email = document.getElementById("email");
 const emailValidator = new Validator(email);
 
@@ -14,3 +15,18 @@ emailValidator.addValidCheck((value) => {
 }, "Invalid email (e.g. example@email.com)");
 
 email.addEventListener("blur", emailValidator.isValid);
+
+// Country validation
+const country = document.getElementById("country");
+const countryValidator = new Validator(country);
+
+countryValidator.addValidCheck((value) => {
+  return value.length > 0;
+}, "Please enter a country");
+
+countryValidator.addValidCheck((value) => {
+  const countryRegExp = /^[A-Za-z]+$/;
+  return countryRegExp.test(value);
+}, "Country can only contain letters");
+
+country.addEventListener("blur", countryValidator.isValid);
